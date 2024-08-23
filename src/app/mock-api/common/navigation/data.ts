0de44d8b3af7e4d6559f937fe818508c618673c7
Mijail Gonzalez <mijail0824@gmail.com>
2024-08-23 00:40:50 -0500

    [Sprint 3] ajuste de layout

--- src/app/mock-api/common/navigation/data.ts
+++ src/app/mock-api/common/navigation/data.ts
@@ -2,18 +2,11 @@
 import { FuseNavigationItem } from '@fuse/components/navigation';
 
 export const defaultNavigation: FuseNavigationItem[] = [
-    {
-        id   : 'example',
-        title: 'Example',
-        type : 'basic',
-        icon : 'heroicons_outline:chart-pie',
-        link : '/example'
-    },
-    {
+   {
         id: 'apps.configuraciones',
         title: 'Configuraciones',
         type: 'collapsable',
-        icon: 'settings_input_component',
+        icon: 'settings',
         children: [
             {
                 id: 'apps.ecommerce.inventory',
