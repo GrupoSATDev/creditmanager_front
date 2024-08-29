1f2e70a3dd75a060c5683c7f59fd8d8b3669d097
Mijail Gonzalez <mijail0824@gmail.com>
2024-08-29 12:46:06 -0500

    [Sprint 4] Componente dashboard

---
+++ src/app/pages/dashboard/dashboard.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { DashboardMainComponent } from './dashboard-main/dashboard-main.component';
+
+export default [
+    {
+        path: '',
+        component: DashboardMainComponent
+    }
+
+] as Routes
