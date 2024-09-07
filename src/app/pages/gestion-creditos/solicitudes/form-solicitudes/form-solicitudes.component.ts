44c0a2205e17da342b8000f8d43cf73ab7970047
Mijail Gonzalez <mijail0824@gmail.com>
2024-09-07 00:09:35 -0500

    [Sprint 6] stepper solicitudes

--- src/app/pages/gestion-creditos/solicitudes/form-solicitudes/form-solicitudes.component.ts
+++ src/app/pages/gestion-creditos/solicitudes/form-solicitudes/form-solicitudes.component.ts
@@ -2,13 +2,14 @@ import { Component, inject, OnInit } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { FuseConfirmationService } from '../../../../../@fuse/services/confirmation';
 import { EstadosDatosService } from '../../../../core/services/estados-datos.service';
 import { ToastAlertsService } from '../../../../core/services/toast-alerts.service';
 import { guardar } from '../../../../core/constant/dialogs';
 import { SolicitudesService } from '../../../../core/services/solicitudes.service';
+import { MatStep, MatStepper, MatStepperNext, MatStepperPrevious } from '@angular/material/stepper';
 
 @Component({
   selector: 'app-form-solicitudes',
@@ -19,6 +20,10 @@ import { SolicitudesService } from '../../../../core/services/solicitudes.servic
         MatInput,
         MatLabel,
         ReactiveFormsModule,
+        MatStepper,
+        MatStep,
+        MatStepperPrevious,
+        MatStepperNext,
     ],
   templateUrl: './form-solicitudes.component.html',
   styleUrl: './form-solicitudes.component.scss'
@@ -32,9 +37,24 @@ export class FormSolicitudesComponent implements OnInit{
     public toasService = inject(ToastAlertsService);
     public _matData = inject(MAT_DIALOG_DATA);
 
+    initialInfoForm!: FormGroup;
+    firstFormGroup!: FormGroup;
+    secondFormGroup!: FormGroup;
+
     private solicitudService: SolicitudesService = inject(SolicitudesService)
 
     ngOnInit(): void {
+        this.initialInfoForm = this.fb.group({
+            // No es obligatorio tener validaciones aquí si solo es lectura
+        });
+
+        this.firstFormGroup = this.fb.group({
+            nombre: ['', Validators.required]
+        });
+
+        this.secondFormGroup = this.fb.group({
+            cupo: ['', [Validators.required]]
+        });
         this.createForm();
         const dialogData = this._matData;
         if (dialogData.edit) {
@@ -45,9 +65,9 @@ export class FormSolicitudesComponent implements OnInit{
     }
 
     onSave() {
-        if (this.form.valid) {
+        if (this.secondFormGroup.valid) {
             if (!this._matData.edit) {
-                const data = this.form.getRawValue();
+                const data = this.secondFormGroup.getRawValue();
                 const {cupo,  ...form} = data;
                 const createData = {
                     cupo: Number(cupo),
