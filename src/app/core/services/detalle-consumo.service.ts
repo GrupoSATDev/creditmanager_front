f2f5d3e25df0c5e2706041c59ffb9b494ee68182
Mijail Gonzalez <mijail0824@gmail.com>
2024-09-25 00:32:52 -0500

    [Sprint 7] consumo de detalle consumo

--- src/app/core/services/detalle-consumo.service.ts
+++ src/app/core/services/detalle-consumo.service.ts
@@ -21,13 +21,18 @@ export class DetalleConsumoService {
       return this._http.get(`${this.appSettings.detalleConsumos.url.base}/${id}`);
   }
 
-
-
   postDetalle(data) : Observable<any> {
       return this._http.post(this.appSettings.detalleConsumos.url.base, data);
   }
 
   getResumen(id): Observable<any> {
       return this._http.get(`${this.appSettings.detalleConsumos.url.base}/trabajador/${id}`)
+  }
+
+  patchConsumo(data): Observable<any> {
+    const id = data.id;
+    delete data.id;
+    const {idEstado} = data;
+    return this._http.patch(`${this.appSettings.detalleConsumos.url.base}/${id}?idEstado=${idEstado}`,{} )
   }
 }
--- src/app/pages/gestion-creditos/consumos/form-consumos/form-consumos.component.ts
+++ src/app/pages/gestion-creditos/consumos/form-consumos/form-consumos.component.ts
@@ -2,14 +2,30 @@ import { Component, inject, OnInit } from '@angular/core';
 import { ToastAlertsService } from '../../../../core/services/toast-alerts.service';
 import { FuseConfirmationService } from '../../../../../@fuse/services/confirmation';
 import { EstadosDatosService } from '../../../../core/services/estados-datos.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { DetalleConsumoService } from '../../../../core/services/detalle-consumo.service';
 import { Subscription } from 'rxjs';
+import { CdkScrollable } from '@angular/cdk/scrolling';
+import { CodigosEstadosSolicitudes } from '../../../../core/enums/estados-solicitudes';
+import { CurrencyPipe, DatePipe, NgIf } from '@angular/common';
+import { MatAnchor, MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { CodigosDetalleConsumo } from '../../../../core/enums/detalle-consumo';
+import { guardar } from '../../../../core/constant/dialogs';
 
 @Component({
   selector: 'app-form-consumos',
   standalone: true,
-  imports: [],
+    imports: [
+        CdkScrollable,
+        CurrencyPipe,
+        DatePipe,
+        MatAnchor,
+        MatButton,
+        MatIcon,
+        NgIf,
+        RouterLink,
+    ],
   templateUrl: './form-consumos.component.html',
   styleUrl: './form-consumos.component.scss'
 })
@@ -19,7 +35,9 @@ export class FormConsumosComponent implements  OnInit{
     public estadosDatosService = inject(EstadosDatosService);
     public detalleConsumoService = inject(DetalleConsumoService);
     private activatedRoute = inject(ActivatedRoute);
+    private router = inject(Router);
     idCredito: string = '';
+    public detalle: any;
     public subcription$: Subscription;
 
     ngOnInit(): void {
@@ -30,7 +48,78 @@ export class FormConsumosComponent implements  OnInit{
     getDetalle(id) {
         this.subcription$ = this.detalleConsumoService.getConsumo(id).subscribe((response) => {
             console.log(response)
+            this.detalle = response.data;
         })
     }
 
+    onSave() {
+        const data = {
+            idEstado: CodigosDetalleConsumo.APROBADA,
+            id: this.detalle.id
+        }
+
+        const dialog = this.fuseService.open({
+            ...guardar
+        });
+
+        dialog.afterClosed().subscribe((response) => {
+            if (response === 'confirmed') {
+                this.subcription$ = this.detalleConsumoService.patchConsumo(data).subscribe((response) => {
+                    if (response.isExitoso) {
+                        this.toasService.toasAlertWarn({
+                            message: 'Registro creado con exito!',
+                            actionMessage: 'Cerrar',
+                            duration: 3000
+                        })
+                        this.router.navigate(['/pages/gestion-creditos/consumos']);
+                        this.estadosDatosService.stateGridSolicitudes.next({state: true, tab: 0});
+                    }
+                }, error => {
+                    this.toasService.toasAlertWarn({
+                        message: 'Ha ocurrido un error!!!!',
+                        actionMessage: 'Cerrar',
+                        duration: 3000
+                    })
+                })
+            }
+        })
+
+
+    }
+
+    onReject() {
+        const data = {
+            idEstado: CodigosDetalleConsumo.RECHAZADA,
+            id: this.detalle.id
+        }
+
+        const dialog = this.fuseService.open({
+            ...guardar
+        });
+
+        dialog.afterClosed().subscribe((response) => {
+            if (response === 'confirmed') {
+                this.subcription$ = this.detalleConsumoService.patchConsumo(data).subscribe((response) => {
+                    if (response.isExitoso) {
+                        this.toasService.toasAlertWarn({
+                            message: 'Registro creado con exito!',
+                            actionMessage: 'Cerrar',
+                            duration: 3000
+                        })
+                        this.router.navigate(['/pages/gestion-creditos/consumos']);
+                        this.estadosDatosService.stateGridSolicitudes.next({state: true, tab: 0});
+                    }
+                }, error => {
+                    this.toasService.toasAlertWarn({
+                        message: 'Ha ocurrido un error!!!!',
+                        actionMessage: 'Cerrar',
+                        duration: 3000
+                    })
+                })
+            }
+        })
+
+    }
+
+    protected readonly CodigosDetalleConsumo = CodigosDetalleConsumo;
 }
