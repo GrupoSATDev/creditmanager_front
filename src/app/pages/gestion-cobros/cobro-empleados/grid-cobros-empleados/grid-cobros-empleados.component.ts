d575e7057b10108e4234ac84875d374f47d32845
Mijail Gonzalez <mijail0824@gmail.com>
2024-09-27 23:09:24 -0500

    [Sprint 7] estilos botones

--- src/app/pages/gestion-cobros/cobro-empleados/grid-cobros-empleados/grid-cobros-empleados.component.ts
+++ src/app/pages/gestion-cobros/cobro-empleados/grid-cobros-empleados/grid-cobros-empleados.component.ts
@@ -1,12 +1,58 @@
 import { Component } from '@angular/core';
+import { CustomTableComponent } from '../../../shared/custom-table/custom-table.component';
+import { MatButton } from '@angular/material/button';
+import { MatFormField } from '@angular/material/form-field';
+import { MatIcon } from '@angular/material/icon';
+import { MatInput } from '@angular/material/input';
+import { Subscription } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { IButton } from '../../../shared/interfaces/buttonsInterfaces';
 
 @Component({
   selector: 'app-grid-cobros-empleados',
   standalone: true,
-  imports: [],
+    imports: [
+        CustomTableComponent,
+        MatButton,
+        MatFormField,
+        MatIcon,
+        MatInput,
+    ],
   templateUrl: './grid-cobros-empleados.component.html',
   styleUrl: './grid-cobros-empleados.component.scss'
 })
 export class GridCobrosEmpleadosComponent {
+    public subcription$: Subscription;
+    public selectedData: any;
+    public searchTerm: string = '';
+
+    data = [];
+
+    columns = ['Fecha cuota', 'Valor cuota', 'Valor pagado', 'Estado'];
+
+    columnPropertyMap = {
+        'Nit': 'nit',
+        'Razon social': 'razonSocial',
+        'Correo': 'correo',
+        'Telefono': 'telefono',
+        'Direccion': 'direccion'
+    };
+
+    buttons: IButton[] = [
+        {
+            label: 'View',
+            icon: 'visibility',
+            action: (element) => {
+                console.log('Editing', element);
+                this.selectedData = element;
+            }
+        },
+    ];
+
+    constructor(
+        private _matDialog: MatDialog,
+
+    ) {
+    }
 
 }
