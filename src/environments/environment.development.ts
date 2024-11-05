5e230a8af1fe2115fd1030295f44caf71ba3c148
Mijail Gonzalez <mijail0824@gmail.com>
2024-11-04 22:37:07 -0500

    [Sprint 12] deployment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,8 @@
 export const environment = {
+    //urlApi: 'https://backend.crediplus.com.co/api/',
     //urlApi: 'https://api.crediplus.com.co/api/',
     urlApi: 'https://localhost:7032/api/',
     baseUrl: 'https://localhost:7032/api/',
     //baseUrl: 'https://api.crediplus.com.co/api/',
+    //baseUrl: 'https://backend.crediplus.com.co/api/',
 };
