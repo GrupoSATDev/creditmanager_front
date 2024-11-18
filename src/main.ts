7901665c6da93ee493ddd026e0c7f36f16e3b2eb
Mijail Gonzalez <49327662+mgonzalez0889@users.noreply.github.com>
2024-11-18 12:22:05 -0500

    [Sprint 12] pwa feature

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,16 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from 'app/app.component';
 import { appConfig } from 'app/app.config';
 
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/ngsw-worker.js').then((registration) => {
+            console.log('Service Worker registrado con éxito:', registration);
+        }).catch((error) => {
+            console.error('Error en el registro del Service Worker:', error);
+        });
+    });
+}
+
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
     console.error(err)
 );
