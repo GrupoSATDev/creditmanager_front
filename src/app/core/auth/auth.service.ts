b780825177110ff357455705de182afa6f5bcce8
Mijail Gonzalez <49327662+mgonzalez0889@users.noreply.github.com>
2024-11-18 14:25:05 -0500

    [Sprint 12] refrescando optimziado

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -213,8 +213,8 @@ export class AuthService {
         }
 
         // If the access token exists, and it didn't expire, sign in using it
-        return this.signInUsingToken();
-        //return of(true);
+        //return this.signInUsingToken();
+        return of(true);
 
     }
 
