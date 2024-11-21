5eaf51ae59b4aebc92906ce6750932ca04731d08
Mijail Gonzalez <49327662+mgonzalez0889@users.noreply.github.com>
2024-11-20 22:03:24 -0500

    [Sprint 12] update serviceworker and pwa

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,17 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { provideServiceWorker } from '@angular/service-worker';
+import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-    let component: AppComponent;
-    let fixture: ComponentFixture<AppComponent>;
-
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             imports: [AppComponent],
-            providers: [
-                provideServiceWorker('ngsw-worker.js', {
-                    enabled: true
-                })
-            ]
         }).compileComponents();
-
-        fixture = TestBed.createComponent(AppComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
     });
 
-    it('should do something', async () => {
-        // Correcto
-        await fixture.whenStable().then().then(() => {
-            // Código que se ejecutará cuando el DOM esté estable
-        });
-        // Tu código aquí
+    it('should create the app', () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.componentInstance;
+        expect(app).toBeTruthy();
     });
+
 });
