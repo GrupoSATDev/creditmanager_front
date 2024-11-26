78b4415f386ccf2eb72e2fd27118cde897a89a0f
Mijail Gonzalez <49327662+mgonzalez0889@users.noreply.github.com>
2024-11-26 10:50:49 -0500

    [Sprint 12] cobros adicionales fijos

--- src/app/pages/gestion-creditos/creditos/dialog-costos-adicionales/dialog-costos-adicionales.component.ts
+++ src/app/pages/gestion-creditos/creditos/dialog-costos-adicionales/dialog-costos-adicionales.component.ts
@@ -3,7 +3,7 @@ import { MatButton } from '@angular/material/button';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { IConfig, NgxMaskDirective, provideNgxMask } from 'ngx-mask';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { DateAdapter, MAT_DATE_LOCALE, MatOption } from '@angular/material/core';
 import { DateAdapterService } from '../../../../core/services/date-adapter.service';
 import { AsyncPipe, CurrencyPipe, DatePipe, NgForOf, NgIf } from '@angular/common';
@@ -13,10 +13,11 @@ import { EstadosDatosService } from '../../../../core/services/estados-datos.ser
 import { SwalService } from '../../../../core/services/swal.service';
 import { MatSelect, MatSelectChange } from '@angular/material/select';
 import { CobrosFijosService } from '../../../../core/services/cobros-fijos.service';
-import { tap } from 'rxjs';
+import { map, tap } from 'rxjs';
 import { MatList, MatListItem } from '@angular/material/list';
 import { idCobro } from '../../../../core/constant/cobro-fijo';
 import { DetalleConsumoService } from '../../../../core/services/detalle-consumo.service';
+import { guardar } from '../../../../core/constant/dialogs';
 
 const maskConfig: Partial<IConfig> = {
     validation: false,
@@ -63,38 +64,80 @@ export class DialogCostosAdicionalesComponent {
     private readonly destroyedRef = inject(DestroyRef);
     private cobrosFijosService = inject(CobrosFijosService)
     private detalleConsumoService = inject(DetalleConsumoService)
+    public cobroFijo: FormControl = new FormControl('');
 
 
-    public costosFijos$ = this.cobrosFijosService.getCobros()
-    labelData: any;
+    public costosFijos$ = this.cobrosFijosService.getCobros().pipe(
+        tap((response) => {
+            const cantidadCuotas = this._matData.data.cantCuotas;
+            const cobroSugerido = this.obtenerCobroSugerido(response.data, cantidadCuotas);
 
+            if (!this.cobroFijo.value) {
+                this.cobroFijo.setValue(cobroSugerido);
+            }
+        })
+
+    )
+
+    private obtenerCobroSugerido(cobros: any[], cantidadCuotas: number): any {
+        const rangos = [
+            { min: 0, max: 30, indice: 0 },
+            { min: 31, max: 60, indice: 1 },
+            { min: 61, max: 90, indice: 2 },
+            { min: 91, max: 120, indice: 3 }
+        ];
+
+        const rangoSeleccionado = rangos.find(
+            rango => cantidadCuotas >= rango.min && cantidadCuotas <= rango.max
+        );
+
+        return rangoSeleccionado
+            ? cobros[rangoSeleccionado.indice]
+            : cobros[0];
+    }
 
     onSave() {
         const data = this._matData.data;
-        console.log(data)
+        const {id} = this.cobroFijo.value;
         const createData = {
             idCredito: data.id,
             idTipoConsumo: idCobro,
             idTrabajador: data.idTrabajador,
-            idCobroFijo: this.labelData.id,
+            idCobroFijo: id,
         }
 
-        console.log(createData)
-        this.detalleConsumoService.postConsumoFijo(createData).subscribe((response) => {
-            console.log(response)
+        const dialog = this.fuseService.open({
+            ...guardar
+        });
+
+        dialog.afterClosed().subscribe((response) => {
+
+            if (response === 'confirmed') {
+                this.detalleConsumoService.postConsumoFijo(createData).subscribe((response) => {
+                    console.log(response)
+                    this.estadosDatosService.stateGrid.next(true);
+                    this.swalService.ToastAler({
+                        icon: 'success',
+                        title: 'Registro Creado o Actualizado con Exito.',
+                        timer: 4000,
+                    })
+                    this.closeDialog();
+                }, error => {
+                    this.swalService.ToastAler({
+                        icon: 'error',
+                        title: 'Ha ocurrido un error al crear el registro!',
+                        timer: 4000,
+                    })
+                })
+            }else {
+                this.closeDialog();
+            }
         })
 
     }
 
     closeDialog() {
         this.dialogRef.close();
-    }
-
-    onSelected(event: MatSelectChange) {
-        const data = event.value;
-        this.labelData = data;
-        console.log(data)
-
     }
 
 
