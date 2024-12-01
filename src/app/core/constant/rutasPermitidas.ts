45d9b58d40765f0ea178757d704fb5ae55270862
Mijail Gonzalez <49327662+mgonzalez0889@users.noreply.github.com>
2024-12-01 00:03:33 -0500

    [Sprint 13] redirigir al role especifico

---
+++ src/app/core/constant/rutasPermitidas.ts
@@ -0,0 +1,5 @@
+export const RoleRouteMap: { [key: string]: string } = {
+    'Super Admin': '/pages/gestion-creditos/solicitudes',
+    'Analista': '/pages/gestion-creditos/solicitudes',
+    'Aliado': '/pages/gestion-creditos/solicitudes',
+};
