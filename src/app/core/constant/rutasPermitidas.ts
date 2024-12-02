053d73fd4982af1f4bc128fc6248fd77c4008fd7
Mijail Gonzalez <49327662+mgonzalez0889@users.noreply.github.com>
2024-12-02 18:11:56 -0500

    [Sprint 13] rutas permitidas

--- src/app/core/constant/rutasPermitidas.ts
+++ src/app/core/constant/rutasPermitidas.ts
@@ -1,5 +1,5 @@
 export const RoleRouteMap: { [key: string]: string } = {
     'Super Admin': '/pages/gestion-creditos/solicitudes',
-    'Analista': '/pages/gestion-creditos/solicitudes',
-    'Aliado': '/pages/gestion-creditos/solicitudes',
+    'Analista': '/pages/gestion-creditos/desembolsos',
+    'Aliado': '/pages/gestion-creditos/detalle-consumo',
 };
