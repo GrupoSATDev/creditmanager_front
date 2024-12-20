d64cc3f7b9e3e8fefc16166d9b64cf1c0c2128e7
Mijail Gonzalez <49327662+mgonzalez0889@users.noreply.github.com>
2024-12-20 09:23:37 -0500

    [Sprint 14] rol nuevo acceso

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,9 +1,9 @@
 export const environment = {
     production: false,
-    //urlApi: 'https://backend.crediplus.com.co/api/',
-    urlApi: 'https://api.crediplus.com.co/api/',
+    urlApi: 'https://backend.crediplus.com.co/api/',
+    //urlApi: 'https://api.crediplus.com.co/api/',
     //urlApi: 'https://localhost:7032/api/',
     //baseUrl: 'https://localhost:7032/api/',
-    baseUrl: 'https://api.crediplus.com.co/api/',
-    //baseUrl: 'https://backend.crediplus.com.co/api/',
+    //baseUrl: 'https://api.crediplus.com.co/api/',
+    baseUrl: 'https://backend.crediplus.com.co/api/',
 };
