052c7df8c1e44a547a6b507320126c056eb7ff1a
Mijail Gonzalez <mijail0824@gmail.com>
2025-04-02 21:56:54 -0500

    [sprint 18] analista rol analista solicitudes

--- src/app/core/constant/rutasPermitidas.ts
+++ src/app/core/constant/rutasPermitidas.ts
@@ -1,6 +1,6 @@
 export const RoleRouteMap: { [key: string]: string } = {
     'Super Admin': '/pages/gestion-creditos/solicitudes',
-    'Analista': '/pages/gestion-creditos/desembolsos',
+    'Analista': '/pages/gestion-creditos/solicitudes',
     'Aliado': '/pages/gestion-creditos/detalle-consumo',
     'Desembolso': '/pages/gestion-creditos/desembolsos',
     'Auditor': '/pages/gestion-creditos/solicitudes',
