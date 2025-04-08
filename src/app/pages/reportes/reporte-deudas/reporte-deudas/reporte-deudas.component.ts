0b5de972f08a05cab297b78f87273329eff7901a
Mijail Gonzalez <mijail0824@gmail.com>
2025-04-08 18:12:50 -0500

    [sprint 18] campos adicionales reporte sin liquidar

--- src/app/pages/reportes/reporte-deudas/reporte-deudas/reporte-deudas.component.ts
+++ src/app/pages/reportes/reporte-deudas/reporte-deudas/reporte-deudas.component.ts
@@ -68,6 +68,8 @@ export class ReporteDeudasComponent implements OnInit {
         'Intereses a la fecha',
         'Valor cuota',
         'Deuda costos',
+        'Recaudado',
+        'Estado'
     ];
     columnsLiquidado = [
         'Fecha de desembolso',
@@ -88,6 +90,8 @@ export class ReporteDeudasComponent implements OnInit {
         'Intereses a la fecha': 'deudaIntereses',
         'Valor cuota': 'valorCuota',
         'Deuda costos': 'deudaCobroFijo',
+        'Recaudado': 'recaudado',
+        'Estado': 'estado',
     };
     columnPropertyLiquidadoMap = {
         'Fecha de desembolso': 'fechaDesembolso',
@@ -138,6 +142,12 @@ export class ReporteDeudasComponent implements OnInit {
                         );
                         items.deudaTotal = this.currencyPipe.transform(
                             items.deudaTotal,
+                            'USD',
+                            'symbol',
+                            '1.2-2'
+                        );
+                        items.recaudado = this.currencyPipe.transform(
+                            items.recaudado,
                             'USD',
                             'symbol',
                             '1.2-2'
@@ -227,6 +237,12 @@ export class ReporteDeudasComponent implements OnInit {
             }
             if (this.selectedTab == '') {
                 mappedItem.DeudaCostos = parseCurrency(items.deudaCobroFijo)
+            }
+            if (this.selectedTab == '') {
+                mappedItem.Recaudado = parseCurrency(items.recaudado)
+            }
+            if (this.selectedTab == '') {
+                mappedItem.Estado = items.estado
             }
             if (this.selectedTab == CodigoEstadosCreditosLiquidados.LIQUIDADO) {
                 mappedItem.PagosPendientes = items.pagosPendientes;
