f45b177a24bbb5bc0907dc88b391106bbd466211
Mijail Gonzalez <mijail0824@gmail.com>
2025-05-05 22:41:37 -0500

    [sprint 18] exportar datos cobros

--- src/app/pages/gestion-cobros/pago-trabajador/grid-pago-trabajador/grid-pago-trabajador.component.ts
+++ src/app/pages/gestion-cobros/pago-trabajador/grid-pago-trabajador/grid-pago-trabajador.component.ts
@@ -4,7 +4,7 @@ import { MatButton } from '@angular/material/button';
 import { MatFormField } from '@angular/material/form-field';
 import { MatIcon } from '@angular/material/icon';
 import { MatInput } from '@angular/material/input';
-import { CurrencyPipe, DatePipe, NgIf } from '@angular/common';
+import { CurrencyPipe, DatePipe, NgClass, NgIf } from '@angular/common';
 import { Subscription, tap } from 'rxjs';
 import { PagoTrabajadoresService } from '../../../../core/services/pago-trabajadores.service';
 import { MatDialog } from '@angular/material/dialog';
@@ -16,11 +16,15 @@ import { MatTab, MatTabChangeEvent, MatTabContent, MatTabGroup } from '@angular/
 import { CodigoCobroTrabajador } from '../../../../core/enums/codigo-cobro-trabajador';
 import { DialogEstadoComponent } from '../dialog-estado/dialog-estado.component';
 import { DialogAbonoIndividualComponent } from '../dialog-abono-individual/dialog-abono-individual.component';
+import { exportar } from '../../../../core/constant/dialogs';
+import * as XLSX from 'xlsx';
+import { FuseConfirmationService } from '../../../../../@fuse/services/confirmation';
+import { parseCurrency } from '../../../../core/utils/number-utils';
 
 
 @Component({
-  selector: 'app-grid-pago-trabajador',
-  standalone: true,
+    selector: 'app-grid-pago-trabajador',
+    standalone: true,
     imports: [
         CustomTableComponent,
         MatButton,
@@ -32,13 +36,11 @@ import { DialogAbonoIndividualComponent } from '../dialog-abono-individual/dialo
         MatTabContent,
         MatTabGroup,
         NgIf,
+        NgClass,
     ],
-    providers: [
-        DatePipe,
-        CurrencyPipe
-    ],
-  templateUrl: './grid-pago-trabajador.component.html',
-  styleUrl: './grid-pago-trabajador.component.scss'
+    providers: [DatePipe, CurrencyPipe],
+    templateUrl: './grid-pago-trabajador.component.html',
+    styleUrl: './grid-pago-trabajador.component.scss',
 })
 export class GridPagoTrabajadorComponent implements OnInit, OnDestroy {
     private datePipe = inject(DatePipe);
@@ -49,21 +51,30 @@ export class GridPagoTrabajadorComponent implements OnInit, OnDestroy {
 
     private pagoTrabajadorService = inject(PagoTrabajadoresService);
     private _matDialog = inject(MatDialog);
-    private estadoDatosService = inject(EstadosDatosService)
+    private estadoDatosService = inject(EstadosDatosService);
     private router = inject(Router);
     private selectedTab: any = CodigoCobroTrabajador.PENDIENTES;
-    public tabIndex ;
+    public tabIndex;
+    public fuseService = inject(FuseConfirmationService);
 
     data = [];
+    exportData = [];
 
-    columns = ['Número de pago', 'Identificación', 'Nombres Apellidos', 'Empresa', 'Total', 'Estado' ];
+    columns = [
+        'Número de pago',
+        'Identificación',
+        'Nombres Apellidos',
+        'Empresa',
+        'Total',
+        'Estado',
+    ];
     columnPropertyMap = {
         'Número de pago': 'consecutivo',
-        'Identificación': 'numDocumento',
+        Identificación: 'numDocumento',
         'Nombres Apellidos': 'nombreTrabajador',
-        'Empresa': 'nombreSubempresa',
-        'Total': 'total',
-        'Estado': 'nombreEstadoCobro',
+        Empresa: 'nombreSubempresa',
+        Total: 'total',
+        Estado: 'nombreEstadoCobro',
     };
 
     buttons: IButton[] = [
@@ -72,8 +83,11 @@ export class GridPagoTrabajadorComponent implements OnInit, OnDestroy {
             icon: 'visibility',
             action: (element) => {
                 console.log('Editing', element);
-                this.router.navigate(['/pages/gestion-cobros/trabajador/individual/', element.id])
-            }
+                this.router.navigate([
+                    '/pages/gestion-cobros/trabajador/individual/',
+                    element.id,
+                ]);
+            },
         },
         {
             label: 'Cambiar estado',
@@ -82,7 +96,7 @@ export class GridPagoTrabajadorComponent implements OnInit, OnDestroy {
                 console.log('View', element);
                 this.selectedData = element;
                 this.onCambioEstado();
-            }
+            },
         },
         {
             label: 'Abonos',
@@ -91,7 +105,7 @@ export class GridPagoTrabajadorComponent implements OnInit, OnDestroy {
                 console.log('View', element);
                 this.selectedData = element;
                 this.onAbono();
-            }
+            },
         },
     ];
 
@@ -101,23 +115,25 @@ export class GridPagoTrabajadorComponent implements OnInit, OnDestroy {
             icon: 'visibility',
             action: (element) => {
                 console.log('Editing', element);
-                this.router.navigate(['/pages/gestion-cobros/trabajador/individual/', element.id])
-            }
+                this.router.navigate([
+                    '/pages/gestion-cobros/trabajador/individual/',
+                    element.id,
+                ]);
+            },
         },
-    ]
+    ];
 
     onCambioEstado(): void {
         this._matDialog.open(DialogEstadoComponent, {
             autoFocus: false,
             data: {
                 edit: true,
-                data: this.selectedData
+                data: this.selectedData,
             },
             width: '35%',
             disableClose: true,
-            panelClass: 'custom-dialog-container'
-        })
-
+            panelClass: 'custom-dialog-container',
+        });
     }
 
     onAbono(): void {
@@ -125,24 +141,66 @@ export class GridPagoTrabajadorComponent implements OnInit, OnDestroy {
             autoFocus: false,
             data: {
                 edit: true,
-                data: this.selectedData
+                data: this.selectedData,
             },
             width: '35%',
             disableClose: true,
-            panelClass: 'custom-dialog-container'
-        })
+            panelClass: 'custom-dialog-container',
+        });
     }
-
-
 
     tabChanged = (tabChangeEvent: MatTabChangeEvent): void => {
         this.tabIndex = tabChangeEvent.index;
-        this.selectedTab = tabChangeEvent.index == 0 ? CodigoCobroTrabajador.PENDIENTES : CodigoCobroTrabajador.PAGADOS;
+        this.selectedTab =
+            tabChangeEvent.index == 0
+                ? CodigoCobroTrabajador.PENDIENTES
+                : CodigoCobroTrabajador.PAGADOS;
         this.getPagoTrabajadores(this.selectedTab);
+    };
+
+    exportToExcel(data: any[]) {
+        const dialog = this.fuseService.open({
+            ...exportar,
+        });
+
+        dialog.afterClosed().subscribe((response) => {
+            if (response === 'confirmed') {
+                // Create worksheet
+                const worksheet = XLSX.utils.json_to_sheet(data);
+
+                // Create workbook
+                const workbook = XLSX.utils.book_new();
+                XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
+
+                // Export file
+                XLSX.writeFile(
+                    workbook,
+                    this.selectedTab == CodigoCobroTrabajador.PENDIENTES ?
+                        `Registro de cobro individual Pendientes${this.datePipe.transform(new Date(), 'dd/MM/yyyy')}.xlsx`
+                        :
+                        `Registro de cobro individual Pagados${this.datePipe.transform(new Date(), 'dd/MM/yyyy')}.xlsx`
+                );
+            }
+        });
     }
 
-    ngOnDestroy(): void {
+    private convertDataExport(data) {
+        const convertData = data.map((items) => {
+            const mappedItem: any = {
+                NúmeroDePago: items.consecutivo,
+                Identificación: items.numDocumento,
+                NombresApellidos: items.nombreTrabajador,
+                Empresa: items.nombreSubempresa,
+                Valor: parseCurrency(items.total),
+                Estado: items.nombreEstadoCobro,
+            };
+            return mappedItem;
+
+        });
+        this.exportData = convertData;
     }
+
+    ngOnDestroy(): void {}
 
     ngOnInit(): void {
         this.getPagoTrabajadores(this.selectedTab);
@@ -150,35 +208,44 @@ export class GridPagoTrabajadorComponent implements OnInit, OnDestroy {
     }
 
     onNew() {
-        this.router.navigate(['/pages/gestion-cobros/trabajador/individual'])
+        this.router.navigate(['/pages/gestion-cobros/trabajador/individual']);
     }
 
     public getPagoTrabajadores(params) {
-        this.pagoTrabajadorService.getPagoTrabajadorIndividual(params).pipe(
-            tap((response) => {
-                response.data.forEach((items) => {
-                    items.fechaCreacion = this.datePipe.transform(items.fechaCreacion, 'dd/MM/yyyy');
-                    items.total = this.currencyPipe.transform(items.total, 'USD', 'symbol', '1.2-2');
-                    //items.nombreTrabajador = this.datePipe.transform(items.nombreTrabajador, 'titlecase');
+        this.pagoTrabajadorService
+            .getPagoTrabajadorIndividual(params)
+            .pipe(
+                tap((response) => {
+                    response.data.forEach((items) => {
+                        items.fechaCreacion = this.datePipe.transform(
+                            items.fechaCreacion,
+                            'dd/MM/yyyy'
+                        );
+                        items.total = this.currencyPipe.transform(
+                            items.total,
+                            'USD',
+                            'symbol',
+                            '1.2-2'
+                        );
+                        //items.nombreTrabajador = this.datePipe.transform(items.nombreTrabajador, 'titlecase');
+                    });
+                    return response;
                 })
-                return response;
-            })
-
-        ).subscribe((response) => {
-            if (response.data) {
-                this.data = response.data;
-            }
-
-
-        })
+            )
+            .subscribe((response) => {
+                if (response.data) {
+                    this.data = response.data;
+                    this.convertDataExport(response.data);
+                } else {
+                    this.data = [];
+                }
+            });
     }
 
     onSearch(event: Event) {
         const target = event.target as HTMLInputElement;
         this.searchTerm = target.value.trim().toLowerCase();
     }
-
-
 
     private listenGrid() {
         const refreshData$ = this.estadoDatosService.stateGrid.asObservable();
@@ -187,8 +254,6 @@ export class GridPagoTrabajadorComponent implements OnInit, OnDestroy {
             if (state) {
                 this.getPagoTrabajadores(this.selectedTab);
             }
-        })
-
+        });
     }
-
 }
--- src/app/pages/gestion-cobros/pago-trabajadores/grid-pago-trabajadores/grid-pago-trabajadores.component.ts
+++ src/app/pages/gestion-cobros/pago-trabajadores/grid-pago-trabajadores/grid-pago-trabajadores.component.ts
@@ -4,7 +4,7 @@ import { MatButton } from '@angular/material/button';
 import { MatFormField } from '@angular/material/form-field';
 import { MatIcon } from '@angular/material/icon';
 import { MatInput } from '@angular/material/input';
-import { CurrencyPipe, DatePipe, NgIf } from '@angular/common';
+import { CurrencyPipe, DatePipe, NgClass, NgIf } from '@angular/common';
 import { Subscription, tap } from 'rxjs';
 import { PagoAliadosService } from '../../../../core/services/pago-aliados.service';
 import { MatDialog } from '@angular/material/dialog';
@@ -17,10 +17,14 @@ import { MatTab, MatTabChangeEvent, MatTabContent, MatTabGroup } from '@angular/
 import { CodigoCobroTrabajador } from '../../../../core/enums/codigo-cobro-trabajador';
 import { DialogEstadoMasivoComponent } from '../dialog-estado-masivo/dialog-estado-masivo.component';
 import { DialogAbonosMasivosComponent } from '../dialog-abonos-masivos/dialog-abonos-masivos.component';
+import { exportar } from '../../../../core/constant/dialogs';
+import * as XLSX from 'xlsx';
+import { FuseConfirmationService } from '../../../../../@fuse/services/confirmation';
+import { parseCurrency } from '../../../../core/utils/number-utils';
 
 @Component({
-  selector: 'app-grid-pago-trabajadores',
-  standalone: true,
+    selector: 'app-grid-pago-trabajadores',
+    standalone: true,
     imports: [
         CustomTableComponent,
         MatButton,
@@ -32,15 +36,13 @@ import { DialogAbonosMasivosComponent } from '../dialog-abonos-masivos/dialog-ab
         MatTabContent,
         MatTabGroup,
         NgIf,
+        NgClass,
     ],
-    providers: [
-        DatePipe,
-        CurrencyPipe
-    ],
-  templateUrl: './grid-pago-trabajadores.component.html',
-  styleUrl: './grid-pago-trabajadores.component.scss'
+    providers: [DatePipe, CurrencyPipe],
+    templateUrl: './grid-pago-trabajadores.component.html',
+    styleUrl: './grid-pago-trabajadores.component.scss',
 })
-export class GridPagoTrabajadoresComponent implements OnInit, OnDestroy{
+export class GridPagoTrabajadoresComponent implements OnInit, OnDestroy {
     private datePipe = inject(DatePipe);
     public subcription$: Subscription;
     public selectedData: any;
@@ -49,20 +51,28 @@ export class GridPagoTrabajadoresComponent implements OnInit, OnDestroy{
 
     private pagoTrabajadorService = inject(PagoTrabajadoresService);
     private _matDialog = inject(MatDialog);
-    private estadoDatosService = inject(EstadosDatosService)
+    private estadoDatosService = inject(EstadosDatosService);
     private router = inject(Router);
     private selectedTab: any = CodigoCobroTrabajador.PENDIENTES;
-    public tabIndex ;
+    public fuseService = inject(FuseConfirmationService);
+    public tabIndex;
 
     data = [];
+    exportData = [];
 
-    columns = ['Fecha de liquidación', 'Número de pago',  'Empresa', 'Valor', 'Estado' ];
+    columns = [
+        'Fecha de liquidación',
+        'Número de pago',
+        'Empresa',
+        'Valor',
+        'Estado',
+    ];
     columnPropertyMap = {
         'Fecha de liquidación': 'fechaCreacion',
         'Número de pago': 'consecutivo',
-        'Empresa': 'nombreSubempresa',
-        'Valor': 'total',
-        'Estado': 'nombreEstadoCobro',
+        Empresa: 'nombreSubempresa',
+        Valor: 'total',
+        Estado: 'nombreEstadoCobro',
     };
 
     buttons: IButton[] = [
@@ -71,8 +81,11 @@ export class GridPagoTrabajadoresComponent implements OnInit, OnDestroy{
             icon: 'visibility',
             action: (element) => {
                 console.log('Editing', element);
-                this.router.navigate(['/pages/gestion-cobros/trabajadores/pago/', element.id])
-            }
+                this.router.navigate([
+                    '/pages/gestion-cobros/trabajadores/pago/',
+                    element.id,
+                ]);
+            },
         },
         {
             label: 'Cambiar estado',
@@ -81,7 +94,7 @@ export class GridPagoTrabajadoresComponent implements OnInit, OnDestroy{
                 console.log('View', element);
                 this.selectedData = element;
                 this.onCambioEstado();
-            }
+            },
         },
         {
             label: 'Abonos',
@@ -90,15 +103,18 @@ export class GridPagoTrabajadoresComponent implements OnInit, OnDestroy{
                 console.log('View', element);
                 this.selectedData = element;
                 this.onAbono();
-            }
+            },
         },
         {
             label: 'Comprobante',
             icon: 'document_scanner',
             action: (element) => {
                 console.log('Editing', element);
-                this.router.navigate(['/pages/gestion-cobros/trabajador/individual/', element.id])
-            }
+                this.router.navigate([
+                    '/pages/gestion-cobros/trabajador/individual/',
+                    element.id,
+                ]);
+            },
         },
     ];
 
@@ -108,16 +124,22 @@ export class GridPagoTrabajadoresComponent implements OnInit, OnDestroy{
             icon: 'visibility',
             action: (element) => {
                 console.log('Editing', element);
-                this.router.navigate(['/pages/gestion-cobros/trabajadores/pago/', element.id])
-            }
+                this.router.navigate([
+                    '/pages/gestion-cobros/trabajadores/pago/',
+                    element.id,
+                ]);
+            },
         },
         {
             label: 'Comprobante',
             icon: 'document_scanner',
             action: (element) => {
                 console.log('Editing', element);
-                this.router.navigate(['/pages/gestion-cobros/trabajador/individual/', element.id])
-            }
+                this.router.navigate([
+                    '/pages/gestion-cobros/trabajador/individual/',
+                    element.id,
+                ]);
+            },
         },
     ];
 
@@ -126,12 +148,12 @@ export class GridPagoTrabajadoresComponent implements OnInit, OnDestroy{
             autoFocus: false,
             data: {
                 edit: true,
-                data: this.selectedData
+                data: this.selectedData,
             },
             width: '35%',
             disableClose: true,
-            panelClass: 'custom-dialog-container'
-        })
+            panelClass: 'custom-dialog-container',
+        });
     }
 
     onAbono(): void {
@@ -139,22 +161,50 @@ export class GridPagoTrabajadoresComponent implements OnInit, OnDestroy{
             autoFocus: false,
             data: {
                 edit: true,
-                data: this.selectedData
+                data: this.selectedData,
             },
             width: '35%',
             disableClose: true,
-            panelClass: 'custom-dialog-container'
-        })
+            panelClass: 'custom-dialog-container',
+        });
     }
 
     tabChanged = (tabChangeEvent: MatTabChangeEvent): void => {
         this.tabIndex = tabChangeEvent.index;
-        this.selectedTab = tabChangeEvent.index == 0 ? CodigoCobroTrabajador.PENDIENTES : CodigoCobroTrabajador.PAGADOS;
+        this.selectedTab =
+            tabChangeEvent.index == 0
+                ? CodigoCobroTrabajador.PENDIENTES
+                : CodigoCobroTrabajador.PAGADOS;
         this.getPagoTrabajadores(this.selectedTab);
+    };
+
+    exportToExcel(data: any[]) {
+        const dialog = this.fuseService.open({
+            ...exportar,
+        });
+
+        dialog.afterClosed().subscribe((response) => {
+            if (response === 'confirmed') {
+                // Create worksheet
+                const worksheet = XLSX.utils.json_to_sheet(data);
+
+                // Create workbook
+                const workbook = XLSX.utils.book_new();
+                XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
+
+                // Export file
+                XLSX.writeFile(
+                    workbook,
+                    this.selectedTab == CodigoCobroTrabajador.PENDIENTES ?
+                    `Registro de cobro masivo Pendientes${this.datePipe.transform(new Date(), 'dd/MM/yyyy')}.xlsx`
+                    :
+                    `Registro de cobro masivo Pagados${this.datePipe.transform(new Date(), 'dd/MM/yyyy')}.xlsx`
+                );
+            }
+        });
     }
 
-    ngOnDestroy(): void {
-    }
+    ngOnDestroy(): void {}
 
     ngOnInit(): void {
         this.getPagoTrabajadores(this.selectedTab);
@@ -162,35 +212,63 @@ export class GridPagoTrabajadoresComponent implements OnInit, OnDestroy{
     }
 
     onNew() {
-        this.router.navigate(['/pages/gestion-cobros/trabajadores/pago'])
+        this.router.navigate(['/pages/gestion-cobros/trabajadores/pago']);
     }
 
     public getPagoTrabajadores(params) {
-        this.pagoTrabajadorService.getPagosTrabajadores(params).pipe(
-            tap((response) => {
-                response.data.forEach((items) => {
-                    items.fechaCreacion = this.datePipe.transform(items.fechaCreacion, 'dd/MM/yyyy');
-                    items.total = params == CodigoCobroTrabajador.PENDIENTES ?  items.total - items.totalAbono : items.total;
-                    items.total = this.currencyPipe.transform(items.total, 'USD', 'symbol', '1.2-2');
-                    //items.nombreTrabajador = this.datePipe.transform(items.nombreTrabajador, 'titlecase');
+        this.pagoTrabajadorService
+            .getPagosTrabajadores(params)
+            .pipe(
+                tap((response) => {
+                    response.data.forEach((items) => {
+                        items.fechaCreacion = this.datePipe.transform(
+                            items.fechaCreacion,
+                            'dd/MM/yyyy'
+                        );
+                        items.total =
+                            params == CodigoCobroTrabajador.PENDIENTES
+                                ? items.total - items.totalAbono
+                                : items.total;
+                        items.total = this.currencyPipe.transform(
+                            items.total,
+                            'USD',
+                            'symbol',
+                            '1.2-2'
+                        );
+                        //items.nombreTrabajador = this.datePipe.transform(items.nombreTrabajador, 'titlecase');
+                    });
+                    return response;
                 })
-                return response;
-            })
+            )
+            .subscribe((response) => {
+                if (response.data) {
+                    this.data = response.data;
+                    this.convertDataExport(response.data);
+                } else {
+                    this.data = [];
+                }
+            });
+    }
 
-        ).subscribe((response) => {
+    private convertDataExport(data) {
+        const convertData = data.map((items) => {
+            const mappedItem: any = {
+                FechaDeLiquidacion: items.fechaCreacion,
+                NúmeroDePago: items.consecutivo,
+                Empresa: items.nombreSubempresa,
+                Valor: parseCurrency(items.total),
+                Estado: items.nombreEstadoCobro,
+            };
+            return mappedItem;
 
-            this.data = response.data;
-
-        })
+        });
+        this.exportData = convertData;
     }
 
     onSearch(event: Event) {
         const target = event.target as HTMLInputElement;
         this.searchTerm = target.value.trim().toLowerCase();
     }
-
-
-
 
     private listenGrid() {
         const refreshData$ = this.estadoDatosService.stateGrid.asObservable();
@@ -199,8 +277,6 @@ export class GridPagoTrabajadoresComponent implements OnInit, OnDestroy{
             if (state) {
                 this.getPagoTrabajadores(this.selectedTab);
             }
-        })
-
+        });
     }
-
 }
