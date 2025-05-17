93705423cac7115082e9141d3c2e74d785028fc1
Juan Castillo <51374452+jdcsalej@users.noreply.github.com>
2025-05-17 14:58:40 -0500

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,9 +1,9 @@
 export const environment = {
     production: false,
     //urlApi: 'https://backend.crediplus.com.co/api/',
-    urlApi: 'https://api.crediplus.com.co/api/',
+    urlApi: 'https://crediplusapi.sinergya.co/',
     //urlApi: 'https://localhost:7032/api/',
     //baseUrl: 'https://localhost:7032/api/',
-    baseUrl: 'https://api.crediplus.com.co/api/',
+    baseUrl: 'https://crediplusapi.sinergya.co/',
     //baseUrl: 'https://backend.crediplus.com.co/api/',
 };
